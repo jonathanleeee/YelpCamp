c47211e21b8630fcc7fd9edf8c22bba35c367827
Jonathan Lee <jonathanleeee@gmail.com>
2017-12-12 23:05:31 +0000

    create a model directory, use Module.exports, require campground.js

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 var express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
-    mongoose   = require("mongoose");
+    mongoose   = require("mongoose"),
+    Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//SCHEMA set up
-var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String,
-   description:  String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 // {
