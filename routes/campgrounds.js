29766bd2eeb87366b51a3b0583d4d704017e4c3c
Jonathan Lee <jonathanleeee@gmail.com>
2017-12-19 05:04:46 +0000

    prevent an unauthenticatd user from creating a campground, save username + id to newly created background

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,24 +14,29 @@ router.get("/", function(req, res){
 });
 
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn,function(req, res){
    var name = req.body.name;
    var image = req.body.image;
    var desc = req.body.description;
-   var newCampground = {name: name, image: image, description: desc};
+   var author = {
+       id: req.user._id,
+       username: req.user.username
+   };
+   var newCampground = {name: name, image: image, description: desc, author: author};
    
    Campground.create(newCampground, function(err, newlyCreated){
        //create new campground and save to DB
       if(err){
           console.log(err);
       } else {
+          console.log(newlyCreated);
           res.redirect("/campgrounds");
       }
    });
 });
 
 //NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn,function(req, res){
    res.render("campgrounds/new");
 });
 
@@ -48,5 +53,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+      return next();
+  }  
+  res.redirect("/login");
+}
 
 module.exports = router;
