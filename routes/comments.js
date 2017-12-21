7ca8d16992ed5df94f0003d65a3a1089ace1e5cb
Jonathan Lee <jonathanleeee@gmail.com>
2017-12-21 23:21:28 +0000

    add destroy route and delete button for comment

--- routes/comments.js
+++ routes/comments.js
@@ -66,6 +66,18 @@ router.put("/:comment_id", function(req, res){
    }); 
 });
 
+//DESTROY route for comment
+router.delete("/:comment_id", function(req, res){
+   //findByIdAndRemove
+   Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");         
+       } else {
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
