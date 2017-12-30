b81f10f9ff11eb24c0549f9114e8b95597e05ea0
Jonathan Lee <jonathanleeee@gmail.com>
2017-12-30 23:04:01 +0000

    add dynamic price to new.ejs, edit.ejs, show.ejs, modify campground create route to accept price

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 //SCHEMA set up
 var campgroundSchema = new mongoose.Schema({
    name: String,
+   price: String, 
    image: String,
    description:  String,
    author: {
