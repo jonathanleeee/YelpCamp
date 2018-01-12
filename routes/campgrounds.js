b6723a61c0900578f8d0f4137913103d5725bb2f
Jonathan Lee <jonathanleeee@gmail.com>
2018-01-12 02:36:27 +0000

    add admin feature, cast error haven't solve

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -80,6 +80,7 @@ router.put("/:id", function(req, res){
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, campground){
         if(err){
+            console.log(err);
             req.flash("error", err.message);
             res.redirect("back");
         } else {
